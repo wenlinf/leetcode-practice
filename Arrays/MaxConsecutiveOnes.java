918eef6cc49070285c5a9df2cfe4ca20eb88527f
wenlinf <wenlin.fang9664@gmail.com>
2020-12-27 10:42:17 +0800

    another solution

--- Arrays/MaxConsecutiveOnes.java
+++ Arrays/MaxConsecutiveOnes.java
@@ -14,3 +14,26 @@ class MaxConsecutiveOnes {
         return count;
     }
 }
+
+
+//A better solution from: https://medium.com/@saurav.agg19/max-consecutive-ones-c419cc842ae4
+// class Solution {
+//     public int findMaxConsecutiveOnes(int[] nums) {
+//         int max = 0;
+//         int current = 0;
+//         for (int i : nums) {
+//             if (i == 1) {
+//                 current++;
+//             } else {
+//                 if (current > max) {
+//                     max = current;
+//                 }
+//                 if (max >= nums.length / 2) {
+//                     return max;
+//                 }
+//                 current = 0;
+//             }
+//         }
+//         return Math.max(max, current);
+//     }
+// }
