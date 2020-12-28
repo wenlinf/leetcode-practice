8482c5908b78936a3898493e8c4b75e634508d7d
wenlinf <42773608+wenlinf@users.noreply.github.com>
2020-12-28 22:30:53 +0800

    first solution
    to remove duplicate elements in a sorted array

---
+++ Arrays/RemoveDuplicates.java
@@ -0,0 +1,15 @@
+class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+        int pointer = 1;
+        while (pointer < nums.length) {
+            if (nums[pointer] == nums[count]) {
+                pointer++;
+            } else {
+                count++;
+                nums[count] = nums[pointer];
+            }
+        }
+        return ++count;
+    }
+}
