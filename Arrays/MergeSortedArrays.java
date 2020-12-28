c0fade136a37534a8190ac268661219676a83e1f
wenlinf <42773608+wenlinf@users.noreply.github.com>
2020-12-28 13:01:28 +0800

    added link to a second solution

--- Arrays/MergeSortedArrays.java
+++ Arrays/MergeSortedArrays.java
@@ -23,3 +23,5 @@ class MergeSortedArrays {
         }
     }
 }
+
+//one solution using two pointers: https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3253/discuss/989064/Two-pointer-solution-wvideo-whiteboard-explanation
