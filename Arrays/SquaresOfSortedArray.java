b192f003bdebc80c1bacdcf96247e34d5ee02106
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-01-04 10:40:39 +0800

    Create SquaresOfSortedArray.java
    https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3574/

---
+++ Arrays/SquaresOfSortedArray.java
@@ -0,0 +1,17 @@
+class SquaresOfSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int[] result = new int[nums.length];
+        for (int i = nums.length - 1; i >= 0; i--) {
+             if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                 result[i] = nums[right] * nums[right];
+                 right--;
+            }
+        }
+        return result;
+    }
+}
