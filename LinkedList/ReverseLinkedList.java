1df97e48431acba3689cdad4afc466bf88b33bb5
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-01-09 16:13:32 +0800

    recursive solution
    https://leetcode.com/problems/reverse-linked-list/submissions/

--- LinkedList/ReverseLinkedList.java
+++ LinkedList/ReverseLinkedList.java
@@ -21,4 +21,11 @@ class ReverseLinkedList {
         }
         return head;
     }
+     public ListNode reverseListRecursive(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
 }
