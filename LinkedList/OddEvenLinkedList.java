8b3b4e16f2656f169d828aa3b4f6333d4ba6a2c8
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-01-09 17:54:54 +0800

    added a prettier solution
    Prettier solution from: https://leetcode.com/problems/odd-even-linked-list/solution/

--- LinkedList/OddEvenLinkedList.java
+++ LinkedList/OddEvenLinkedList.java
@@ -31,4 +31,18 @@ class OddEvenLinkedList {
         even.next = null;
         return head;
     }
+
+    public ListNode oddEvenListPrettierSolution(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+
 }
