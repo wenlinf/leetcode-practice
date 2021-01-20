b274cda3f9f7da36a3e3e202eae913bc7daaf837
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-01-20 14:30:18 +0800

    Create BinaryTreePostorderTraversal.java
    https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/930/

---
+++ BinaryTree/BinaryTreePostorderTraversal.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        if (root.left != null) result.addAll(postorderTraversal(root.left));
+        if (root.right != null) result.addAll(postorderTraversal(root.right));
+        result.add(root.val);
+        return result;
+    }
+}
