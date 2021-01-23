b9cd58f0e4546c9fcbc04d2c6afad292c5509cf6
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-01-23 20:16:10 +0800

    Create ConstructBinaryTreeFromInorderAndPostorderTraversal.java
    https://leetcode.com/explore/learn/card/data-structure-tree/133/conclusion/942/

---
+++ BinaryTree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class ConstructBinaryTreeFromInorderAndPostorderTraversal {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder.length == 0 || postorder.length == 0) return null;
+        if (inorder.length == 1 || postorder.length == 1) return new TreeNode(inorder[0]);
+        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
+        int i;
+        for (i = 0; i < inorder.length; i++) {
+            if (inorder[i] == root.val) break;
+        }
+        int[] subLeftTreeInorder;
+        int[] subRightTreeInorder;
+        int[] subLeftTreePostorder;
+        int[] subRightTreePostorder;
+        if (i == inorder.length - 1) {
+            root.right = null;
+            subLeftTreeInorder = Arrays.copyOfRange(inorder, 0, inorder.length - 1);
+            subLeftTreePostorder = Arrays.copyOfRange(postorder, 0, postorder.length - 1);
+        } else {
+            subRightTreeInorder = Arrays.copyOfRange(inorder, i+1, inorder.length);
+            subRightTreePostorder = Arrays.copyOfRange(postorder, i, postorder.length - 1);
+            root.right = buildTree(subRightTreeInorder, subRightTreePostorder);
+            subLeftTreeInorder = Arrays.copyOfRange(inorder, 0, i);
+            subLeftTreePostorder = Arrays.copyOfRange(postorder, 0, i);
+        }
+        root.left = buildTree(subLeftTreeInorder, subLeftTreePostorder);          
+        return root;
+    }
+}
