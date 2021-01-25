22f99d0ab33556a0fbb3cb27eaa5c5c5bd653df7
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-01-25 22:27:18 +0800

    Create ContainsDuplicate.java
    https://leetcode.com/problems/contains-duplicate/

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,10 @@
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) return true;
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
