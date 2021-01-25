dd162635548fa59092a3cf2e9acc1a519af231e8
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-01-25 22:14:57 +0800

    Create PlusOne.java
    https://leetcode.com/problems/plus-one/
    Runtime: 0 ms
    Memory Usage: 37.3 MB

---
+++ Arrays/PlusOne.java
@@ -0,0 +1,22 @@
+class PlusOne {
+    public int[] plusOne(int[] digits) {
+        if(digits[digits.length - 1] != 9) {
+            digits[digits.length - 1] = digits[digits.length - 1] + 1;
+            return digits;
+        }
+        digits[digits.length - 1] = 0;
+        for (int i = digits.length - 2; i >=0; i--) {
+                if (digits[i] != 9) {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+        }
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        for (int j = 0; j < digits.length; j++) {
+            res[j+1] = digits[j];
+        }
+        return res;
+    }
+}
