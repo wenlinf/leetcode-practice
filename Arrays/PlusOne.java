1fa93d86df38674700b99371a7d504fa717d9150
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-01-25 22:20:58 +0800

    Update PlusOne.java
    removed unnecessary lines

--- Arrays/PlusOne.java
+++ Arrays/PlusOne.java
@@ -1,11 +1,6 @@
 class PlusOne {
     public int[] plusOne(int[] digits) {
-        if(digits[digits.length - 1] != 9) {
-            digits[digits.length - 1] = digits[digits.length - 1] + 1;
-            return digits;
-        }
-        digits[digits.length - 1] = 0;
-        for (int i = digits.length - 2; i >=0; i--) {
+        for (int i = digits.length - 1; i >= 0; i--) {
                 if (digits[i] != 9) {
                     digits[i]++;
                     return digits;
