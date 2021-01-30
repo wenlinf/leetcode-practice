3cb07e333c62878e65542615f8b0e42f0659a1a6
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-01-30 16:54:28 +0800

    Create ReverseString.java
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/

---
+++ String/ReverseString.java
@@ -0,0 +1,14 @@
+class ReverseString {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+        char temp;
+        while (start < end) {
+            temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+         }
+    }
+}
