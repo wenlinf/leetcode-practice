327f770b768ea4164db1e080fa8f17b0fc117e9c
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-02-02 23:16:16 +0800

    Update FirstBadVersion.java

--- Searching/FirstBadVersion.java
+++ Searching/FirstBadVersion.java
@@ -6,7 +6,7 @@ public class Solution extends VersionControl {
         int start = 1;
         int end = n;
         while (start < end) {
-            int mid = (start + end) / 2;
+            int mid = start + (end - start) / 2;
             if (isBadVersion(mid)) {
                  if (!isBadVersion(mid - 1)) return mid;
                  end = mid;
