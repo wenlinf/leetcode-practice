1c003e79e83e5f40129ea57d08e1b9ff61b26630
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-02-02 23:30:18 +0800

    Update FirstBadVersion.java

--- Searching/FirstBadVersion.java
+++ Searching/FirstBadVersion.java
@@ -6,7 +6,7 @@ public class Solution extends VersionControl {
         int start = 1;
         int end = n;
         while (start < end) {
-            int mid = start + (end - start) / 2;
+            int mid = start + (end - start) / 2; // using (start + end) /2 causes overflow
             if (isBadVersion(mid)) {
                  if (!isBadVersion(mid - 1)) return mid;
                  end = mid;
