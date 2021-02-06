5e943d3729c7151845912a2a3373531486b12187
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-02-06 16:16:20 +0800

    Update LengthOfLastWord.java

--- String/LengthOfLastWord.java
+++ String/LengthOfLastWord.java
@@ -2,12 +2,6 @@ class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
         s = s.trim();
         if (s.length() == 0) return 0;
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ' ') stack.push(i);
-        }
-        if (stack.isEmpty()) return s.length();
-        Integer index = stack.pop();
-        return s.length() - 1 - index;
+        return s.length() - 1 - s.lastIndexOf(' ');
     }
 }
