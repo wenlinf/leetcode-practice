03bbbecd19d8304c2e1841318b6870556f8f8946
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-03-07 21:56:43 +0800

    Update SameTree.java

--- BinaryTree/SameTree.java
+++ BinaryTree/SameTree.java
@@ -18,8 +18,6 @@ class SameTree {
         if (p == null && q == null) return true;
         if (p == null || q == null) return false;
         if (p.val != q.val) return false;
-        if (!isSameTree(p.left, q.left)) return false;
-        if (!isSameTree(p.right, q.right)) return false;
-        return true;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
