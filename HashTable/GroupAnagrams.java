56a9f346f148a2056626e69fa490fea353fa1a1d
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-03-24 10:17:42 +0800

    Update GroupAnagrams.java

--- HashTable/GroupAnagrams.java
+++ HashTable/GroupAnagrams.java
@@ -1,5 +1,5 @@
 class GroupAnagrams {
-    public List<List<String>> groupAnagrams(String[] strs) {
+     public List<List<String>> groupAnagrams(String[] strs) {
         String tempStr;
         char[] tempArr;
         Map<String, List<String>> map = new HashMap<>();
@@ -7,16 +7,8 @@ class GroupAnagrams {
             tempArr = strs[i].toCharArray();
             Arrays.sort(tempArr);
             tempStr = String.valueOf(tempArr);
-            if (!map.containsKey(tempStr)) {
-                List<String> list = new ArrayList<>();
-                list.add(strs[i]);
-                map.put(tempStr, list);
-            }
-            else {
-                List<String> currList = map.get(tempStr);
-                currList.add(strs[i]);
-                map.put(tempStr, currList);
-            }
+            if (!map.containsKey(tempStr)) map.put(tempStr, new ArrayList<>());
+            map.get(tempStr).add(strs[i]);
         }
         List<List<String>> result = new ArrayList<>();
         for (Map.Entry<String, List<String>> entry : map.entrySet()){
