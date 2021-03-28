4323dedd20def61506bcfdda855acfaf19c98137
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-03-28 19:12:35 +0800

    Create JewelsAndStones.java
    https://leetcode.com/explore/learn/card/hash-table/187/conclusion-hash-table/1136/

---
+++ HashTable/JewelsAndStones.java
@@ -0,0 +1,14 @@
+class JewelsAndStones {
+    public int numJewelsInStones(String jewels, String stones) {
+        if (jewels.length() == 0) return 0;
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < jewels.length(); i++) {
+            set.add(jewels.charAt(i));
+        }
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            if (set.contains(stones.charAt(i))) count++;
+        }
+        return count;
+    }
+}
