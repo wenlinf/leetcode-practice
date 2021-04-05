fc1e5036576e2dea4adc5fa46c94b9e0511abff4
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-04-05 20:43:00 +0800

    Update ImplementstrStr.java

--- String/ImplementstrStr.java
+++ String/ImplementstrStr.java
@@ -10,4 +10,25 @@ class Solution {
         }
         return index;
     }
+    
+    
+    
+    public int strStr2(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        if (needle.length() > haystack.length()) return -1;
+        for (int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                if (needle.length() == 1) return i;
+                for (int j = 1; j < needle.length(); j++) {
+                    if (i + j >= haystack.length()) return -1;
+                    if (haystack.charAt(i+j) != needle.charAt(j)) {
+                        break;
+                    } 
+                    if (j == needle.length() - 1) return i;
+                }
+            }
+        }
+        return -1;
+    }
+
 }
