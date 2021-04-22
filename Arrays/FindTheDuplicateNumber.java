9b70e1aad9cc9790e8e7a700e9d3e250e7da635d
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-04-22 16:10:53 +0800

    Create FindTheDuplicateNumber.java
    https://leetcode.com/problems/find-the-duplicate-number/

---
+++ Arrays/FindTheDuplicateNumber.java
@@ -0,0 +1,18 @@
+class FindTheDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        if (nums.length == 2) return nums[0];
+        int index = 0;
+        int result = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                result = nums[i];
+                return Math.abs(result);
+            } else {
+                nums[index]*= -1;
+            }
+        }
+        return result;
+    }
+}
