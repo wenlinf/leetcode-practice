04fc9680e5f2fdb6c719757820568f5e5433c513
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-05-31 10:04:34 +0800

    Create SingleNumberII.java
    https://leetcode.com/problems/single-number-ii/

---
+++ TBD/SingleNumberII.java
@@ -0,0 +1,16 @@
+class SingleNumberII {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            if (entry.getValue() == 1) {
+                result = entry.getKey();
+                break;
+            }
+        }
+        return result;
+    }
+}
