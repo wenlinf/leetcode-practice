4fd0ff74749f3000aa44d8800e5e9346129f3004
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-09-15 07:54:59 -0400

    Update ContainsDuplicate.java

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -8,3 +8,16 @@ class ContainsDuplicate {
         return false;
     }
 }
+
+// i don't know why I used map before??? changed to set
+
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
