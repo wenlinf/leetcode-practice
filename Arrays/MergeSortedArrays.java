5939bd1321293eca82f2ec6ed5677cae270bf516
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-09-22 21:17:07 -0400

    Update MergeSortedArrays.java

--- Arrays/MergeSortedArrays.java
+++ Arrays/MergeSortedArrays.java
@@ -48,6 +48,47 @@ class MergeSortedArrays {
             }
         }
     }
+    public static void merge3(int[] nums1, int m, int[] nums2, int n) {
+        //two pointers use m, n and another pointer for nums1
+        n--;
+        m--;
+        int pointer = nums1.length - 1; // https://leetcode.com/problems/merge-sorted-array/discuss/29704/My-clean-java-solution in this solution he used m + n - 1
+        //start looping through the two arrays from the end
+        //first check the el at pointer m and pointer n, put the bigger one at the end of nums1
+        //then decrease the pointer of the array that has the biggest value and compare the next two elements and repeat the steps above
+        //until one of the pointers reaches the start of the array, end the loop
+        while (m >= 0 && n >= 0) {
+            if (nums2[n] >= nums1[m]) {
+                nums1[pointer] = nums2[n];
+                n--;
+                pointer--;
+            } else {
+                nums1[pointer] = nums1[m];
+                m--;
+                pointer--;
+            }
+        }
+        //if there are elements left in nums2, add them all to nums1
+        if (n >= 0) {
+            while (n >= 0) {
+                nums1[pointer] = nums2[n];
+                n--;
+                pointer--;
+            }
+        }
+        /* 
+        in the other solution, the author was using
+        while (m >= 0 && n >= 0) {
+        nums1[pointer--] = (nums1[m] > nums2[n]) ? 
+                             nums1[m--] : nums2[n--];
+    }
+        while (n >= 0) {
+            nums1[pointer--] = nums2[n--];
+        }
+        neater than my code lol
+        also my naming of these variables are horrible, need to improve
+        */
+    }
 }
 
 //one solution using two pointers: https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3253/discuss/989064/Two-pointer-solution-wvideo-whiteboard-explanation
