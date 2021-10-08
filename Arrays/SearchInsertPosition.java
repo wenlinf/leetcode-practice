794ccb2e6b351cfee455fa2d62d2cad6fac21f7c
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-10-08 17:15:32 -0400

    Update SearchInsertPosition.java
    binary search solution

--- Arrays/SearchInsertPosition.java
+++ Arrays/SearchInsertPosition.java
@@ -10,4 +10,21 @@ class SearchInsertPosition {
         }
         return nums.length;
     }
+    
+    public int searchInsert2(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while (start <= end) {
+            mid = (int) Math.ceil((start + end) / 2);
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    } 
 }
