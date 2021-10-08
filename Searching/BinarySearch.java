103fa78ceea3fb760062e626a422642c06d0f5ef
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-10-08 16:56:20 -0400

    Update BinarySearch.java

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -2,16 +2,16 @@ class BinarySearch {
     public int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
-        int mid = (int)Math.ceil((start + end) / 2);
+        int mid;
         while (start <= end) {
+            mid = (int) Math.ceil((start + end) / 2);
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
-            }
-            mid = (int)Math.ceil((start + end) / 2);
+            } 
         }
         return -1;
     }
