21ea8f5dd7091f0c377d25d05dfa4999cdb13886
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-10-10 09:32:48 -0400

    Update RotateArray.java

--- Arrays/RotateArray.java
+++ Arrays/RotateArray.java
@@ -16,4 +16,20 @@ class RotateArray {
             nums[i] = newArr[i];
         }
     }
+    //modify the array in place
+    public void rotate2(int[] nums, int k) {
+        if (nums.length == 1) return;
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse (int[] nums, int start, int end) {
+        int temp;
+        while (start < end) {
+            temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
 }
