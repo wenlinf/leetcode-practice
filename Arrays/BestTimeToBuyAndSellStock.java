5ff492cf0f94e94749b80be016e65204d56962bb
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-10-09 20:17:00 -0400

    Update BestTimeToBuyAndSellStock.java

--- Arrays/BestTimeToBuyAndSellStock.java
+++ Arrays/BestTimeToBuyAndSellStock.java
@@ -12,4 +12,13 @@ class BestTimeToBuyAndSellStock {
         }
         return profit;
     }
+        public int maxProfitII(int[] prices) {
+        int min = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(prices[i] - min, maxProfit);
+            min = Math.min(min, prices[i]);
+        }
+        return maxProfit;
+    }
 }
