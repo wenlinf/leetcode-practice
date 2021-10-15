0036e6720869cd9fe2c7fb9ad8b4c1825e34e6ee
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-10-15 16:05:58 -0400

    Update ValidParenthesis.java

--- String/ValidParenthesis.java
+++ String/ValidParenthesis.java
@@ -18,4 +18,22 @@ class ValidParenthesis {
         if (!stack.empty()) return false;
         return true;
     }  
+    public boolean isValid_101521(String s) {
+        if (s.length() % 2 != 0) return false;
+        List<Character> list = List.of('(', '{', '[', ']', '}', ')');
+        Stack stack = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            if (list.indexOf(s.charAt(i)) < 3) {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.empty()) return false;
+                Character top = (Character)stack.pop();
+                if (list.indexOf(top) + list.indexOf(s.charAt(i)) != 5) {
+                    return false;
+                }
+            }
+        }
+        if (!stack.empty()) return false;
+        return true;             
+    }  
 }
