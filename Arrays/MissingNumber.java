a50f882b9dbb5672e4ebb8b791dfaa3655df699a
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-11-09 09:33:52 -0500

    Update MissingNumber.java
    solution using bit manipulation

--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -10,4 +10,14 @@ class MissingNumber {
         }
         return result;
     }
+    public int missingNumberBitManipulation(int[] nums) {
+        int res = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            res ^= i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
 }
