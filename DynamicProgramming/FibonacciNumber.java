05a6c59960bf2a2b4eba04fbf7f2f563a7abf79c
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-01-07 16:33:29 -0500

    Create FibonacciNumber.java
    https://leetcode.com/problems/fibonacci-number/solution/

---
+++ DynamicProgramming/FibonacciNumber.java
@@ -0,0 +1,18 @@
+class FibonacciNumber {
+    public int fib(int n) {
+        if (n <= 1) return n;
+        int[] cache = new int[n + 1];
+        cache[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            cache[i] = cache[i - 1] + cache[i - 2];
+        }
+        
+        return cache[n];
+    }
+  
+    public int fib_recursive(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
