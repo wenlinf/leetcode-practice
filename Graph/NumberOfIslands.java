a1414daf4d3cf59d36a7f44ed64888db0d67586a
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-02-02 11:44:57 -0500

    Update NumberOfIslands.java
    added DFS solution

--- Graph/NumberOfIslands.java
+++ Graph/NumberOfIslands.java
@@ -42,3 +42,53 @@ class NumberOfIslands {
         return neighbors;
     }
 }
+
+
+
+class Solution-DFS{
+    private int[][] directions = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    
+    public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        
+        int numIslands = 0;
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(i, j, grid);
+                    numIslands++;
+                }
+            }
+        }
+        return numIslands;
+    }
+    
+    public void dfs(int x, int y, char[][] grid) {
+        grid[x][y] = '0';
+        List<List<Integer>> neighbors = getNeighbors(x, y, grid);
+        for (List<Integer> neighbor: neighbors) {
+            if (grid[neighbor.get(0)][neighbor.get(1)] == '1') {
+                dfs(neighbor.get(0), neighbor.get(1), grid);            
+            }
+        }
+    }
+    
+    public List<List<Integer>> getNeighbors(int x, int y, char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        List<List<Integer>> neighbors = new ArrayList<>();
+        for (int i = 0; i < directions.length; i++) {
+            int row = x + directions[i][0];
+            int col = y + directions[i][1];
+            if (row >= 0 && row < rows && col >= 0 && col < cols) {
+                List<Integer> neighbor = new ArrayList<>();
+                neighbor.add(row);
+                neighbor.add(col);
+                neighbors.add(neighbor);
+            }
+        }
+        return neighbors;
+    }
+}
