1d62f0bfa13190472a4e1ac44dbf796d6cad2cec
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-02-01 19:04:30 -0500

    Update MinStack.java
    another solution using two stacks

--- Stack/MinStack.java
+++ Stack/MinStack.java
@@ -35,3 +35,45 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+
+class MinStack {
+    private Stack<Integer> minStack;
+    private Stack<Integer> tracker;
+
+    public MinStack() {
+        minStack = new Stack<>();
+        tracker = new Stack<>();
+    }
+    
+    public void push(int val) {
+        minStack.push(val);
+        if (tracker.isEmpty() || val <= tracker.peek()) {
+                tracker.push(val);
+        }  
+    }
+    
+    public void pop() {
+        if (tracker.peek().equals(minStack.peek())) {
+            tracker.pop();
+        }
+        minStack.pop();
+    }
+    
+    public int top() {
+        return minStack.peek();
+    }
+    
+    public int getMin() {
+        return tracker.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
