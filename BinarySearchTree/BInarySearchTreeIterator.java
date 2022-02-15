40ed1cad3e751dbf6ad032995f781283e738c6f9
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-02-15 08:52:30 -0500

    added iterative solution

--- BinarySearchTree/BInarySearchTreeIterator.java
+++ BinarySearchTree/BInarySearchTreeIterator.java
@@ -53,3 +53,54 @@ class BSTIterator {
  * int param_1 = obj.next();
  * boolean param_2 = obj.hasNext();
  */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    Stack<TreeNode> stack;
+    
+    public BSTIterator(TreeNode root) {
+        this.stack = new Stack<TreeNode>();
+        leftMostInorder(root);
+    }
+    
+    private void leftMostInorder(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+    }
+    
+    public int next() {
+        TreeNode top = this.stack.pop();
+        if (top.right != null) {
+            this.leftMostInorder(top.right);
+        }
+        return top.val;
+    }
+    
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
