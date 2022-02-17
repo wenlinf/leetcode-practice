5e5b1e61953eb8107c98c5b4fd03afc4cad0f150
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-02-17 16:12:18 -0500

    updated union find solution
    this one is slightly different from the number of provinces/ number of connected components because we have to sort the array first

--- Graph/TheEarliestMomentWhenEveryoneBecomeFriends.java
+++ Graph/TheEarliestMomentWhenEveryoneBecomeFriends.java
@@ -1,70 +1,80 @@
-class TheEarliestMomentWhenEveryoneBecomeFriends {
+class Solution {
     public int earliestAcq(int[][] logs, int n) {
-        Arrays.sort(logs, (a, b) -> a[0] - b[0]);
-        
-        int[] root = new int[n];
-        int[] rank = new int[n];
-        for (int i = 0; i < n; i++) {
-            root[i] = i;
-            rank[i] = 0;
+        // timestamp is not sorted 
+        //symmetric -- undirected
+        // x y pair only occurs once
+        // if there's only one log, return the timestamp
+        if (logs.length == 1) {
+            return logs[0][0];
         }
+        // sort the logs by timestamp
+        Arrays.sort(logs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] log1, int[] log2) {
+                int timeStamp1 = log1[0];
+                int timeStamp2 = log2[0];
+                return timeStamp1 - timeStamp2;
+            }
+        });
+        // can use lambda
+        // Arrays.sort(logs, (a, b) -> a[0] - b[0]);
         
-        int components = n;
+        // create a union find instance
+        UnionFind unionFind = new UnionFind(n);
+        // traverse the logs array
         for (int i = 0; i < logs.length; i++) {
-            boolean isMerged = union(logs[i][1], logs[i][2], root, rank);
-            if (isMerged) components--;
-            if (components == 1) return logs[i][0];
+            int timestamp = logs[i][0];
+            int first = logs[i][1];
+            int second = logs[i][2];
+            unionFind.union(first, second);
+            if (unionFind.getCount() == 1) {
+                return timestamp;
+            }
         }
-        
+        // union the pair
+        // when the count becomes one, return the timestamp
+        // return -1 at the end if nothing was returned earlier
         return -1;
     }
     
-//     public int find(int x, int[] root) {
-//         int rootNode = x;
-//         while (rootNode != root[rootNode]) {
-//             rootNode = root[rootNode];
-//         }
-//         while (x != rootNode) {
-//             int oldRoot = root[x];
-//             root[x] = rootNode;
-//             x = oldRoot;
-//         }
-//         return rootNode;
-//     }
-    
-    public int find(int person, int[] group) {
-        if (group[person] != person)
-            group[person] = find(group[person], group);
-        return group[person];
-    }
-    
-    // public int union(int x, int y, int[] root) {
-    //     int rootX = find(x, root);
-    //     int rootY = find(y, root);
-    //     if (rootX != rootY) {
-    //         root[rootX] = rootY;
-    //     }
-    //     return rootY;
-    // }
-    
-        public boolean union(int a, int b, int[] group, int[] rank) {
-            int groupA = find(a, group);
-            int groupB = find(b, group);
-            boolean isMerged = false;
-
-            if (groupA == groupB) return isMerged;
-            
-            isMerged = true;
-            // Merge the lower-rank group into the higher-rank group.
-            if (rank[groupA] > rank[groupB]) {
-                group[groupB] = groupA;
-            } else if (rank[groupA] < rank[groupB]) {
-                group[groupA] = groupB;
-            } else {
-                group[groupA] = groupB;
-                rank[groupB] += 1;
+    public class UnionFind {
+        private int[] root;
+        private int[] rank;
+        private int count;
+        
+        public UnionFind(int size) {
+            this.root = new int[size];
+            this.rank = new int[size];
+            this.count = size;
+            for (int i = 0; i < size; i++) {
+                this.root[i] = i;
+                this.rank[i] = 1;
             }
-
-            return isMerged;
+        }
+        
+        public int find(int x) {
+            if (x == this.root[x]) return x;
+            return root[x] = find(root[x]);
+        }
+        
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (rank[rootX] > rank[rootY]) {
+                    root[rootY] = rootX;
+                } else if (rank[rootX] < rank[rootY]) {
+                    root[rootX] = rootY;
+                } else {
+                    root[rootX] = rootY;
+                    rank[rootY]++;
+                }
+                this.count--;
+            }
+        }
+        
+        public int getCount() {
+            return this.count;
+        }
     }
 }
