bc458ce0e7bfe73e0de82850a6657eb4e7138867
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-02-17 15:07:56 -0500

    union find solution
    time complexity O(n)
    space complexity O(n)

--- Graph/GraphValidTree.java
+++ Graph/GraphValidTree.java
@@ -44,3 +44,74 @@ class GraphValidTree {
         }
     }
 }
+
+
+
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        // there are no repeated edges, no self loops
+        // if n == 1 it is a valid tree
+        if (n == 1) return true;
+        // create a unionFind instance
+        UnionFind unionFind = new UnionFind(n);
+        // traverse the edges array and union the vertices
+        for (int i = 0; i < edges.length; i++) {
+            int first = edges[i][0];
+            int second = edges[i][1];
+            // if two nodes are already connected, return false
+            if (unionFind.connected(first, second)) {
+                return false;
+            }
+            unionFind.union(first, second);
+        }
+        // if the number of edges (number of union operations) is not n-1, return false
+        return unionFind.getEdges() == n - 1;
+        // else return true
+    }
+    
+    public class UnionFind {
+        private int[] root;
+        private int[] rank;
+        private int size;
+        private int edges;
+        
+        public UnionFind(int size) {
+            this.root = new int[size];
+            this.rank = new int[size];
+            this.edges = 0;
+            for (int i = 0; i < size; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+        
+        public int find(int x) {
+            if (root[x] == x) return x;
+            return root[x] = find(root[x]);
+        }
+        
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (rank[rootX] < rank[rootY]) {
+                    root[rootX] = rootY;
+                } else if(rank[rootX] > rank[rootY]) {
+                    root[rootY] = rootX;
+                } else {
+                    root[rootX] = rootY;
+                    rank[rootY]++;
+                }
+                edges++;
+            }
+        }
+        
+        public boolean connected(int x, int y) {
+            return find(x) == find(y);
+        }
+        
+        public int getEdges() {
+            return this.edges;
+        }
+    }
+}
