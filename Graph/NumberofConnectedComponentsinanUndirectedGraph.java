48f9bca1c9c31756d711c4b31d887db242b68f4f
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-02-17 15:33:11 -0500

    updated union find solution
    time complexity: O(E) E- number of edges
    space complexity: O(V) V- number of vertices

--- Graph/NumberofConnectedComponentsinanUndirectedGraph.java
+++ Graph/NumberofConnectedComponentsinanUndirectedGraph.java
@@ -1,35 +1,59 @@
-class NumberofConnectedComponentsinanUndirectedGraph {
+class Solution {
     public int countComponents(int n, int[][] edges) {
-        int[] root = new int[n];
-        for (int i = 0; i < n; i++) {
-            root[i] = i; 
+        // same as number of provinces
+        // no repeated edges
+        if (n == 1) return 1;
+        // create unionFInd instance
+        UnionFind unionFind = new UnionFind(n);
+        // traverse the edges array
+        for (int i = 0; i < edges.length; i++) {
+            int first = edges[i][0];
+            int second = edges[i][1];
+            // connect the two vertices in each sub array
+            unionFind.union(first, second);
+        }
+        // return the count
+        return unionFind.getCount();
+    }
+    
+    public class UnionFind {
+        private int[] root;
+        private int[] rank;
+        private int count;
+        
+        public UnionFind(int size) {
+            this.root = new int[size];
+            this.rank = new int[size];
+            this.count = size;
+            for (int i = 0; i < size; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
         }
         
-        for (int i = 0; i < edges.length; i++) {
-            union(edges[i][0], edges[i][1], root);
+        public int find(int x) {
+            if (this.root[x] == x) return x;
+            return this.root[x] = find(root[x]);
         }
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < root.length; i++) {
-            if (root[root[i]] != root[i]) {
-                root[i] = find(root[root[i]], root);
+        
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (rank[rootX] < rank[rootY]) {
+                    root[rootX] = rootY;
+                } else if (rank[rootX] > rank[rootY]) {
+                    root[rootY] = rootX;
+                } else {
+                    root[rootY] = rootX;
+                    rank[rootX]++;
+                }
+                this.count--;
             }
-            set.add(root[i]);
         }
-        return set.size();
-    }
-    
-    public int find(int x, int[] root) {
-        while (root[x] != x) {
-            x = root[x];
-        }
-        return x;
-    }
-    
-    public void union(int x, int y, int[] root) {
-        int rootX = find(x, root);
-        int rootY = find(y, root);
-        if (rootX != rootY) {
-            root[rootY] = rootX;
-        }
+        
+        public int getCount() {
+            return this.count;
+        }  
     }
 }
