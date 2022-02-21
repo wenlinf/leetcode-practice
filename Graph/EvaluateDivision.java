f2712006b1204130c8194f64e1bde64ff22b3bd5
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-02-21 14:10:49 -0500

    added unionfind solution

--- Graph/EvaluateDivision.java
+++ Graph/EvaluateDivision.java
@@ -54,3 +54,79 @@ class EvaluateDivision {
         return result;
     }
 }
+
+
+class Solution {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        UnionFind unionFind = new UnionFind();
+    // traverse the equations list, union the elements
+    for (int i = 0; i < equations.size(); i++) {
+      String first = equations.get(i).get(0);
+      String second = equations.get(i).get(1);
+      unionFind.union(first, second, values[i]);
+    }
+
+    double[] result = new double[queries.size()];
+    // traverse the queries and evaluate the equations
+    for (int i = 0; i < queries.size(); i++) {
+      String first = queries.get(i).get(0);
+      String second = queries.get(i).get(1);
+      if (!unionFind.root.containsKey(first) || !unionFind.root.containsKey(second)) {
+        result[i] = -1;
+      } else if (!unionFind.find(first).getKey().equals(unionFind.find(second).getKey())) {
+        result[i] = -1;
+      } else {
+        Pair<String, Double> root = unionFind.find(first);
+        result[i] = unionFind.root.get(first).getValue() / unionFind.root.get(second).getValue();
+      }
+    }
+    return result;
+    }
+    
+    public class UnionFind {
+        private Map<String, Pair<String, Double>> root;
+        private Map<String, Integer> rank;
+        public Map<String, Map<String, Double>> values;
+        
+        
+        public UnionFind() {
+            this.root = new HashMap<>();
+            this.rank = new HashMap<>();
+            this.values = new HashMap<>();
+        }
+        
+        public Pair<String, Double> find(String x) {
+            if (root.get(x) == null) {
+                root.put(x, new Pair<String, Double>(x, 1.0));
+            }
+            if (root.get(x).getKey().equals(x)) return root.get(x);
+            Pair<String, Double> oldRoot = root.get(x);
+            Pair<String, Double> newEntry = find(root.get(x).getKey());
+            root.put(x, new Pair<String, Double>(newEntry.getKey(), newEntry.getValue() * oldRoot.getValue()));
+            return root.get(x);
+        }
+        
+         public void union(String x, String y, double weight) {
+      Pair<String, Double> rootXPair = find(x);
+      Pair<String, Double> rootYPair = find(y);
+      String rootX = rootXPair.getKey();
+      String rootY = rootYPair.getKey();
+      if (!rootX.equals(rootY)) {
+        if (rank.get(rootX) == null) {
+          rank.put(rootX, 1);
+        }
+        if (rank.get(rootY) == null) {
+          rank.put(rootY, 1);
+        }
+        if (rank.get(rootX) > rank.get(rootY)) {
+          root.put(rootY, new Pair<>(rootX, rootXPair.getValue() /(weight * rootYPair.getValue())));
+        } else if (rank.get(rootX) < rank.get(rootY)) {
+          root.put(rootX, new Pair<>(rootY, (weight * rootYPair.getValue()) / rootXPair.getValue()));
+        } else {
+          root.put(rootX, new Pair<>(rootY, (weight * rootYPair.getValue()) / rootXPair.getValue()));
+          rank.put(rootY, rank.get(rootY) + 1);
+        }
+      }
+    }    
+  }
+}
