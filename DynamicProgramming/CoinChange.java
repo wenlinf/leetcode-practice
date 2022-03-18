c80eabe355318987aadb951bb598aeaae2fd4692
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-03-18 08:09:46 -0400

    Update CoinChange.java
    bottom up solution

--- DynamicProgramming/CoinChange.java
+++ DynamicProgramming/CoinChange.java
@@ -20,3 +20,20 @@ class CoinChange {
         return count[amount - 1];
     }
 }
+
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] table = new int[amount + 1];
+        Arrays.fill(table, amount + 1);
+        table[0] = 0;
+        
+        for (int i = 1; i < amount + 1; i++) {
+            for (int coin : coins) {
+                if (i - coin < 0) continue;
+                table[i] = Math.min(table[i], 1 + table[i - coin]);
+            }
+        } 
+        return table[amount] == amount + 1 ? -1 : table[amount];
+    }
+}
