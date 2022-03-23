5d1162ca0b85dd91af6c0d666a1ead1a6c34f596
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-03-23 11:57:20 -0400

    dp table bottom up solution

--- DynamicProgramming/EditDistance.java
+++ DynamicProgramming/EditDistance.java
@@ -32,3 +32,29 @@ class Solution {
         return dp[i][j];
     }
 }
+
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        int[][] table = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            table[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            table[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    table[i][j] = table[i - 1][j - 1];
+                } else {
+                    table[i][j] = 1 + Math.min(table[i][j - 1], Math.min(table[i - 1][j - 1], table[i - 1][j]));
+                }
+            }
+        }
+        return table[m][n];
+    }
+}
