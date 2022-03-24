d8621521c2be1dc60dc98f037eca6cb66ed5d86f
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-03-24 17:17:14 -0400

    Update RegularExpressionMatching.java
    memoization solution

--- DynamicProgramming/RegularExpressionMatching.java
+++ DynamicProgramming/RegularExpressionMatching.java
@@ -36,3 +36,52 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    private String s;
+    private String p;
+    private Map<String, Boolean> memo;
+    
+    public boolean isMatch(String s, String p) {
+        this.s = s;
+        this.p = p;
+        this.memo = new HashMap<>();
+        return dp(0, 0);
+    }
+    
+    private boolean dp(int i, int j) {
+         if (j == p.length()) {
+            return i == s.length();
+        } else if (i == s.length()) {
+            if ((p.length() - j) % 2 == 1) {
+                return false;
+            } else {
+                for (; j < p.length(); j+=2) {
+                    if (p.charAt(j+1) != '*') {
+                        return false;
+                    } 
+                }
+                return true;
+            }
+        }
+        String key = String.valueOf(i) + "," + String.valueOf(j);
+        if (memo.containsKey(key)) return memo.get(key);
+        boolean result = false;
+       
+        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
+            if (j < p.length() - 1 && p.charAt(j + 1) == '*') {
+                result = dp(i + 1, j) || dp(i, j + 2);
+            } else {
+                result = dp(i+1, j+1);
+            }
+        } else {
+            if (j < p.length() - 1 && p.charAt(j + 1) == '*') {
+                result = dp(i, j + 2);
+            } else {
+                result = false;
+            }
+        }  
+        memo.put(key, result);
+        return result;
+    }
+}
