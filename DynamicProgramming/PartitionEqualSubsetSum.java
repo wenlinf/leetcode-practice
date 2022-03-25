31999c517f6b0223df2eec56e57e70ec06169eab
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-03-25 16:34:21 -0400

    Update PartitionEqualSubsetSum.java
    memoization solution

--- DynamicProgramming/PartitionEqualSubsetSum.java
+++ DynamicProgramming/PartitionEqualSubsetSum.java
@@ -24,3 +24,36 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    // Use memoization based on the recursive solution - accepted
+    private Map<String, Boolean> memo;
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        if (sum % 2 != 0) return false;
+        memo = new HashMap<>();
+        return helper(nums, 0, sum / 2);
+    }
+    
+    private boolean helper(int[] nums, int index, int sum) {
+        String key = String.valueOf(index) + "," + String.valueOf(sum);
+        
+        int n = nums.length;
+        if (index >= n && sum != 0) return false;
+        if (sum <= 0 && index < n) return false;
+        if (index == n && sum == 0) return true;
+        if (memo.containsKey(key)) return memo.get(key);
+        boolean result;
+        if (nums[index] > sum) {
+            result = helper(nums, index + 1, sum);
+        } else {
+            result = helper(nums, index + 1, sum - nums[index]) || helper(nums, index + 1, sum);
+        }
+        memo.put(key, result);
+        return result;
+    }
+}
