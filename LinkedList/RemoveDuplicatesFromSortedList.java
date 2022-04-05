f3dd08ebb1a5387a70b92045d0728969ddb67917
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-04-05 11:33:11 -0400

    Update RemoveDuplicatesFromSortedList.java
    solution using fast and slow pointers

--- LinkedList/RemoveDuplicatesFromSortedList.java
+++ LinkedList/RemoveDuplicatesFromSortedList.java
@@ -22,3 +22,31 @@ class RemoveDuplicatesFromSortedList {
         return head;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return head;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null) {
+            if (fast.val != slow.val) {
+                slow = slow.next;
+                slow.val = fast.val;
+            }
+            fast = fast.next;
+        }
+        slow.next = null;
+        return head;
+    }
+}
