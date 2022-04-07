9bfb2a0695e6690b79fe1837c790535621e9bc3f
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-04-07 08:07:18 -0400

    Update FlattenBInaryTreeToLinkedList.java
    solution using O(1) space

--- BinaryTree/FlattenBInaryTreeToLinkedList.java
+++ BinaryTree/FlattenBInaryTreeToLinkedList.java
@@ -35,3 +35,21 @@ class FlattenBInaryTreeToLinkedList {
         return list;
     }
 }
+
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = null;
+        root.right = left;
+        
+        TreeNode curr = root;
+        while (curr.right != null) {
+            curr = curr.right;
+        }
+        curr.right = right;     
+    }
+}
