5a90743323274fcb2fde2d397d1a48f171fb87b4
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-05-24 17:49:54 -0400

    Update JumpGameII.java

--- Greedy/JumpGameII.java
+++ Greedy/JumpGameII.java
@@ -64,3 +64,20 @@ class Solution {
         }
     } 
 }
+
+//greedy
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int farthest = 0;
+        int end = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == end) {
+                end = farthest;
+                jumps++;
+            }
+        }
+        return jumps;
+    }
+}
