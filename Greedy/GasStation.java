bb650cce1266aef626cfb95e3de6d607aba62626
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-05-25 13:25:15 -0400

    Update GasStation.java

--- Greedy/GasStation.java
+++ Greedy/GasStation.java
@@ -26,3 +26,21 @@ class Solution {
         return -1;
     }
 }
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = cost.length;
+        int sum = 0;
+        int minSum = 0;
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + gas[i] - cost[i];
+            if (sum < minSum) {
+                minSum = sum;
+                start = i + 1;
+            }
+        }
+        if (sum < 0) return -1;
+        return start == n ? 0 : start;
+    }
+}
