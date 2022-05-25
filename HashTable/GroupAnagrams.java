39f8b62b5f3db47630ec93ee2ac7a4a09f2f9dec
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-05-25 11:26:01 -0400

    Update GroupAnagrams.java

--- HashTable/GroupAnagrams.java
+++ HashTable/GroupAnagrams.java
@@ -21,3 +21,30 @@ class GroupAnagrams {
         return result;
     }
 }
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+         Map<String, List<String>> map = new HashMap<>();
+        int[] count = new int[26];
+        for (int i = 0; i < strs.length; i++) {
+            Arrays.fill(count, 0);
+            for (int j = 0; j < strs[i].length(); j++) {
+                char ch = strs[i].charAt(j);
+                count[ch - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int k = 0; k < 26; k++) {
+                sb.append(count[k]);
+                sb.append("#");
+            }
+            String key = sb.toString();
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            
+            map.get(key).add(strs[i]);
+        }
+
+        return new ArrayList(map.values());
+    }
+}
