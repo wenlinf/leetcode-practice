617551ecc6a548eb4aabfeb7732a7331f1b11ae9
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-05-26 15:56:04 -0400

    Update ProductOfArrayExceptSelf.java
    space complexity O(1) solution

--- Arrays/ProductOfArrayExceptSelf.java
+++ Arrays/ProductOfArrayExceptSelf.java
@@ -23,3 +23,20 @@ class ProductOfArrayExceptSelf {
         return result;
     }
 }
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+        answer[0] = 1;
+        for (int i = 1; i < n; i++) {
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = right * answer[i];
+            right *= nums[i];
+        }
+        return answer;
+    }
+}
