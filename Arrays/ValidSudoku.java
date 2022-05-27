149bd08659f156ed4f0081a15f125a5a48060f38
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-05-27 08:53:07 -0400

    Update ValidSudoku.java
    same thing except that Im using a list rather than a map now

--- Arrays/ValidSudoku.java
+++ Arrays/ValidSudoku.java
@@ -31,3 +31,31 @@ class ValidSudoku {
         return true;
     }
 }
+
+// 2020-05-27
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        List<Set<Character>> columnList = new ArrayList<>();
+
+        List<Set<Character>> secList = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            columnList.add(new HashSet<>());
+            secList.add(new HashSet<>());
+        }
+        for (int i = 0; i < 9; i++) {
+            char[] row = board[i];
+            Set<Character> rowSet = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                if (row[j] == '.') continue;
+                if (rowSet.contains(row[j])) return false;
+                
+                if (columnList.size() != 0 && columnList.get(j).size() != 0 && columnList.get(j).contains(row[j])) return false;
+                if (secList.get((i / 3) * 3 + j /3).contains(row[j])) return false;
+                rowSet.add(row[j]);
+                columnList.get(j).add(row[j]);
+                secList.get((i / 3) * 3 + j/3).add(row[j]);
+            }
+        }
+        return true;
+    }
+}
