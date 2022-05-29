744581afd96379f4e4248a511a997678c6f79dc0
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-05-29 18:33:02 -0400

    Create LongestConsecutiveSequence.java
    https://leetcode.com/problems/longest-consecutive-sequence/
    union find solution

---
+++ Arrays/LongestConsecutiveSequence.java
@@ -0,0 +1,69 @@
+class Solution {
+    
+    public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+        UnionFind uf = new UnionFind(n, nums);
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(nums[i])) continue;
+            int num = nums[i];
+            map.put(num, i);
+            if (map.containsKey(num - 1) && !uf.connected(map.get(num), map.get(num - 1))) {
+                uf.union(i, map.get(num - 1));
+            }
+            if (map.containsKey(num + 1) && !uf.connected(map.get(num), map.get(num + 1))) {
+                uf.union(i, map.get(num + 1));
+            }
+        }
+        return uf.getMax();
+    }
+    
+    
+}
+
+class UnionFind {
+    private int[] root;
+    private int[] nodes;
+    
+    public UnionFind(int size, int[] nums) {
+        root = new int[size];
+        nodes = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+            nodes[i] = 1;
+        }
+    }
+    
+    public int find (int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+    
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) return;
+        if (nodes[rootX] > nodes[rootY]) {
+            root[rootY] = rootX;
+            nodes[rootX] += nodes[rootY];
+        } else {
+            root[rootX] = rootY;
+            nodes[rootY] += nodes[rootX];
+        }
+    }
+    
+    public int getMax() {
+        int max = 0;
+        for (int node : nodes) {
+            max = Math.max(max, node);
+        }
+        return max;
+    }
+    
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
+}
