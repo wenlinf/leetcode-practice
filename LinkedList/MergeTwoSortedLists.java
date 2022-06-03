2f19b93068be368ff2c3df76f9570c817c3f7f73
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-06-03 17:45:36 -0400

    Update MergeTwoSortedLists.java
    recursive solution

--- LinkedList/MergeTwoSortedLists.java
+++ LinkedList/MergeTwoSortedLists.java
@@ -35,3 +35,33 @@ class Solution {
         return dummy.next;
     }
 }
+
+// recursive solution
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val > list2.val) {
+           ListNode next = mergeTwoLists(list1, list2.next);
+            list2.next = next;
+            return list2;
+        } else {
+            ListNode next = mergeTwoLists(list1.next, list2);
+            list1.next = next;
+            return list1;
+        }
+        
+    }
+}
