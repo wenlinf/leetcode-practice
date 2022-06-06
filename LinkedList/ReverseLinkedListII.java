57bf77da2225df4545073eb5eca64e5912652a19
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-06-06 16:39:57 -0400

    Update ReverseLinkedListII.java
    iterative solution

--- LinkedList/ReverseLinkedListII.java
+++ LinkedList/ReverseLinkedListII.java
@@ -40,3 +40,48 @@ class Solution {
         return newHead;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == 1) {
+            return reverseN(head, right);
+        }
+        int index = 1;
+        ListNode curr = head;
+        while (index < left - 1) {
+            curr = curr.next;
+            index++;
+        }
+        ListNode newNext = reverseN(curr.next, right - left + 1);
+        curr.next = newNext;
+        return head;
+    }
+    
+    private ListNode successor;
+    private ListNode reverseN(ListNode head, int n) {
+        int index = 1;
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode temp = null;
+        while (index <= n) {
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+            index++;
+        }
+        head.next = temp;
+        return prev;
+    }
+}
