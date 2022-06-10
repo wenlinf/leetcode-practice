aca3fafdaed83fdbff497c31298a58da731618d6
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-06-10 13:21:31 -0400

    Update MeetingRoomII.java

--- Greedy/MeetingRoomII.java
+++ Greedy/MeetingRoomII.java
@@ -25,3 +25,36 @@ class Solution {
         return result;
     }
 }
+
+
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int n = intervals.length;
+        if (n == 1) return 1;
+        
+        int[] begin = new int[n];
+        int[] end = new int[n];
+        for (int i = 0; i < n; i++) {
+            begin[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        Arrays.sort(begin);
+        Arrays.sort(end);
+        
+        int count = 0;
+        int result = 0;
+        int pt1 = 0;
+        int pt2 = 0;
+        while (pt1 < n) {
+            if (begin[pt1] < end[pt2]) {
+                count++;
+                pt1++;
+                result = Math.max(count, result);
+            } else {
+                count--;
+                pt2++;
+            }
+        }
+        return result;
+    }
+}
