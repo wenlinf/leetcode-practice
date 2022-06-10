b159638b75a18dc40ec44488f541177c704779cf
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-06-10 08:22:09 -0400

    Update HandofStraights.java
    treemap

--- Greedy/HandofStraights.java
+++ Greedy/HandofStraights.java
@@ -16,3 +16,28 @@ class Solution {
         return true;
     }
 }
+
+
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n = hand.length;
+        if (n % groupSize != 0) return false;
+        
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(hand[i], map.getOrDefault(hand[i], 0) + 1);
+        }
+        
+        while (map.size() != 0) {
+            int val = map.firstKey();
+            for (int i = 1; i < groupSize; i++) {
+                if (!map.containsKey(val + i)) return false;
+                if (map.get(val+i) == 1) map.remove(val+i);
+                else map.put(val+i, map.get(val+i) - 1);
+            }
+            if (map.get(val) == 1) map.remove(val);
+            else map.put(val, map.get(val) - 1);
+        }
+        return true;
+    }
+}
