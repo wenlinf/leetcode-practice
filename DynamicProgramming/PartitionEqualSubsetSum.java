2847b3f3389c0c6f238199ebfa84e54e287cdd39
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-06-17 19:15:31 -0400

    Update PartitionEqualSubsetSum.java

--- DynamicProgramming/PartitionEqualSubsetSum.java
+++ DynamicProgramming/PartitionEqualSubsetSum.java
@@ -83,3 +83,32 @@ class Solution {
         return dp[n][sum];
     }
 }
+
+// same bottom-up solution but size of array is n rather than n+1
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 == 1) return false;
+        sum = sum / 2;
+        boolean[][] dp = new boolean[n][sum + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (nums[i - 1] == j) {
+                    dp[i][j] = true;
+                } else if (nums[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[n - 1][sum];
+    }
+}
