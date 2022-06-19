5fc3d8a8c8dbebae266e911cd0431af19af92887
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-06-19 07:38:46 -0400

    bottom-up

--- DynamicProgramming/MinimumFallingPathSum.java
+++ DynamicProgramming/MinimumFallingPathSum.java
@@ -29,3 +29,33 @@ class Solution {
         return result;
     }
 }
+
+
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        if (n == 1) return matrix[0][0];
+        int[][] dp = new int[n + 1][n];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    dp[i][j] = Math.min(dp[i - 1][j], 
+                                        dp[i - 1][j + 1]) + matrix[i - 1][j];
+                } else if (j == n - 1) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], 
+                                        dp[i - 1][j]) + matrix[i - 1][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], 
+                                        Math.min(dp[i - 1][j], 
+                                                 dp[i - 1][j + 1])) + matrix[i - 1][j];
+                }
+
+            }
+        }
+        int best = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            best = Math.min(best, dp[n][i]);
+        }
+        return best;
+    }
+}
