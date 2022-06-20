26a66b48d77562a341fa0068fcf1a676cca55290
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-06-20 16:13:19 -0400

    dp bottom up

--- DynamicProgramming/MinimumPathSum.java
+++ DynamicProgramming/MinimumPathSum.java
@@ -25,3 +25,31 @@ class Solution {
         return dp[i][j];
     }
 }
+
+
+// bottom up
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += grid[0][i];
+            dp[0][i] = sum;
+        }
+        sum = 0;
+        for (int i = 0; i < m; i++) {
+            sum += grid[i][0];
+            dp[i][0] = sum;
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
