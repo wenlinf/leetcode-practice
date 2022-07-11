aa7de6df9095a1e8325699ea03bae40ce0d117d1
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-07-11 17:39:20 -0400

    Update InvertBinaryTree.java

--- BinaryTree/InvertBinaryTree.java
+++ BinaryTree/InvertBinaryTree.java
@@ -22,3 +22,14 @@ class InvertBinaryTree {
         return root;
     }
 }
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode newRight = invertTree(root.left);
+        TreeNode newLeft = invertTree(root.right);
+        root.right = newRight;
+        root.left = newLeft;
+        return root;
+    }
+}
