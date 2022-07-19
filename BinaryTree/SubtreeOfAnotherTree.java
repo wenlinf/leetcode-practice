0ac20a1e09b56d402a35f7af6854ff0928fb2ee4
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-07-19 07:25:34 -0400

    Update SubtreeOfAnotherTree.java
    cleaner code

--- BinaryTree/SubtreeOfAnotherTree.java
+++ BinaryTree/SubtreeOfAnotherTree.java
@@ -83,3 +83,33 @@ class Solution {
         return false;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return subRoot == null;
+        if (isSameTree(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+    
+    private boolean isSameTree(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        if (root1.val == root2.val) return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
+        return false;
+    }
+}
