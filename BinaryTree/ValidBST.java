7c86ee458e7f4598eafc096fa5d5fa041cec0dec
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-07-20 11:38:19 -0400

    Update ValidBST.java

--- BinaryTree/ValidBST.java
+++ BinaryTree/ValidBST.java
@@ -32,3 +32,33 @@ class ValidBST {
         return list;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root.left == null && root.right == null) return true;
+        return helper(root, null, null);
+    }
+    
+    private boolean helper(TreeNode root, TreeNode min, TreeNode max) {
+        if (root == null) return true;
+        if (min != null && root.val <= min.val) return false;
+        if (max != null && root.val >= max.val) return false;
+        return helper(root.left, min, root) && helper(root.right, root, max);
+    }
+}
