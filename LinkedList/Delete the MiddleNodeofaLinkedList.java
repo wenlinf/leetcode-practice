80a6b71ffaecc8fd13724a7ad8b0538d2f562fa4
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-07-22 11:14:27 -0400

    Update Delete the MiddleNodeofaLinkedList.java
    two pointers

--- LinkedList/Delete the MiddleNodeofaLinkedList.java
+++ LinkedList/Delete the MiddleNodeofaLinkedList.java
@@ -33,3 +33,30 @@ class Solution {
         return head;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null) return null;
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = slow;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+        prev.next = prev.next.next;
+        return head;
+    }
+}
