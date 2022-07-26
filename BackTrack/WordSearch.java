703004be3f78227d2ef9014cb4a2b00b13ec9743
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-07-25 21:06:29 -0400

    Update WordSearch.java
    clean up code

--- BackTrack/WordSearch.java
+++ BackTrack/WordSearch.java
@@ -3,14 +3,9 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
-        if (m * n < word.length()) return false;
-        if (m == 1 && n == 1) {
-            if (board[0][0] == word.charAt(0)) return true;
-            return false;
-        }
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                char ch = board[i][j];
                 if (dfs(board, word, 0, i, j)) return true;
             }
         }
@@ -18,8 +13,8 @@ class Solution {
     }  
     
     private boolean dfs(char[][] board, String word, int index, int row, int col) {
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) return false;
         if (index >= word.length()) return true;
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) return false;
         
         if (word.charAt(index) != board[row][col]) return false;
         for (int i = 0; i < 4; i++) {
