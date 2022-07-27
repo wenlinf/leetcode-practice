d2ad46845925afe4b5ac4373a95b3fa201c7be88
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-07-27 08:40:29 -0400

    Create SuperEggDrop.java
    https://leetcode.com/problems/super-egg-drop/

---
+++ DynamicProgramming/SuperEggDrop.java
@@ -0,0 +1,27 @@
+// bottom-up dp, Time limit exceeded
+class Solution {
+    public int superEggDrop(int k, int n) {
+         int[][] dp = new int[n + 1][k + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            dp[i][1] = i;
+        }
+        
+        for (int i = 1; i <= k; i++) {
+            dp[1][i] = 1;
+        }  
+        
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j <= k; j++) {
+                int minMoves = Integer.MAX_VALUE;
+                for (int f = 2; f <= i; f++) {
+                    if (Math.max(dp[f - 1][j - 1], dp[i - f][j]) < minMoves) {
+                        minMoves = Math.max(dp[f - 1][j - 1], dp[i - f][j]);
+                        dp[i][j] = minMoves + 1;
+                    }
+                }                
+            }
+        }
+        return dp[n][k];
+    }
+}
