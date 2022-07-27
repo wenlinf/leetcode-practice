60e4b74bce5395553b46722e6c298c801586818e
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-07-27 09:01:08 -0400

    Update SuperEggDrop.java

--- DynamicProgramming/SuperEggDrop.java
+++ DynamicProgramming/SuperEggDrop.java
@@ -25,3 +25,20 @@ class Solution {
         return dp[n][k];
     }
 }
+
+// get min trials from max floors you can try with m trials and k eggs
+class Solution {
+    public int superEggDrop(int k, int n) {
+        int[][] dp = new int[n + 1][k + 1];
+        
+        int m = 0;
+        while (dp[m][k] < n) {
+            m++;
+            for (int i = 1; i <= k; i++) {
+                dp[m][i] = dp[m - 1][i] + dp[m - 1][i - 1] + 1;
+            }
+            
+        }
+        return m;
+    }
+}
