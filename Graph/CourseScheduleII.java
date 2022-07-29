ffcbbc471dc4ef5de77527f9f2f4b54cec91876e
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-07-29 16:01:16 -0400

    Update CourseScheduleII.java

--- Graph/CourseScheduleII.java
+++ Graph/CourseScheduleII.java
@@ -42,3 +42,62 @@ class CourseScheduleII {
         return result;
     }
 }
+
+// DFS solution
+class Solution {
+    private static int WHITE = 1;
+    private static int GRAY = 2;
+    private static int BLACK = 3;
+    private boolean isPossible = true;
+    private Map<Integer, List<Integer>> adjList;
+    private Map<Integer, Integer> color;
+    
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        this.adjList = new HashMap<>();
+        this.color = new HashMap<>();
+        for (int i = 0; i < numCourses; i++) {
+            color.put(i, WHITE);
+        }
+        for (int[] prerequisite : prerequisites) {
+            int course = prerequisite[0];
+            int pre = prerequisite[1];
+            if (!adjList.containsKey(pre)) {
+                adjList.put(pre, new ArrayList<>());
+            }
+            adjList.get(pre).add(course);
+        }
+        Stack<Integer> result = new Stack<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (color.get(i) == WHITE) {
+                dfs(i, result);
+            }
+        }
+        if (!isPossible) return new int[0];
+        int[] res = new int[numCourses];
+        int index = 0;
+        while (!result.isEmpty()) {
+            res[index] = result.pop();
+            index++;
+        }
+        return res;
+    }
+    
+    private void dfs(int start, Stack<Integer> result) {
+        if (!isPossible) return;
+        this.color.put(start, GRAY);
+        if (this.adjList.containsKey(start)) {
+            List<Integer> neighbours = this.adjList.get(start);
+            for (int neighbour : neighbours) {
+                if (color.get(neighbour) == GRAY) {
+                    isPossible = false; 
+                    break;
+                } else if (color.get(neighbour) == WHITE) {
+                    dfs(neighbour, result);
+                }
+            }
+        }
+        
+        this.color.put(start, BLACK);
+        result.add(start);
+    }
+}
