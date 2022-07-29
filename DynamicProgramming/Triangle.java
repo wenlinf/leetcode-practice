df9a0d6fe50c93d2ba54de9f4fe7d18498f2e614
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-07-29 11:29:56 -0400

    Update Triangle.java
    O(n) solution

--- DynamicProgramming/Triangle.java
+++ DynamicProgramming/Triangle.java
@@ -27,3 +27,35 @@ class Solution {
         return result;
     }
 }
+
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        // List<List<Integer>> dp = new ArrayList<>();
+        List<Integer> prev = triangle.get(0);
+        int row = triangle.size();
+
+        // dp.get(0).add(triangle.get(0).get(0));
+        for (int i = 1; i < row; i++) {
+            List<Integer> curr = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                int currNum = triangle.get(i).get(j);
+                if (j == 0) {
+                    curr.add(prev.get(0) + currNum);
+                } else if (j == i) {
+                    curr.add(prev.get(j - 1) + currNum);
+                } else {
+                    curr.add(Math.min(prev.get(j), prev.get(j - 1)) + currNum);
+                }
+            }
+            prev = curr;
+        }
+        int result = Integer.MAX_VALUE;
+        int lastRow = prev.size();
+        
+        for (int i = 0; i < lastRow; i++) {
+            result = Math.min(prev.get(i), result);
+        }
+        return result;
+    }
+}
