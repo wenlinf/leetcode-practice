f1123e8701d99a4455258856c4e9d4967e4b01de
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-07-30 14:29:56 -0400

    Update FindIfPathExistsInGraph.java

--- Graph/FindIfPathExistsInGraph.java
+++ Graph/FindIfPathExistsInGraph.java
@@ -30,3 +30,46 @@ class FindIfPathExistsInGraph {
         return false;
     }
 }
+
+
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if (source == destination) return true;
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int[] edge : edges) {
+            int from = edge[0];
+            int to = edge[1];
+            if (!graph.containsKey(from)) {
+                graph.put(from, new ArrayList<>());
+            }
+            if (!graph.containsKey(to)) {
+                graph.put(to, new ArrayList<>());
+            }
+            graph.get(from).add(to);
+            graph.get(to).add(from);
+        }
+        boolean[] visited = new boolean[n];
+        dfs(source, destination, graph, visited);
+        return exists;
+    }
+    
+    private boolean exists = false;
+    
+    private void dfs(int start, int destination, Map<Integer, List<Integer>> graph, boolean[] visited) {
+        if (visited[start]|| exists) {
+            return;
+        }
+        visited[start] = true;
+        List<Integer> neighbours = graph.get(start);
+        for (int neighbour : neighbours) {
+            if (neighbour == destination) {
+                exists = true;
+                return;
+            }
+            if (!visited[neighbour]) {
+                dfs(neighbour, destination, graph, visited);
+            }        
+        }
+        // visited.removeLast();
+    }
+}
