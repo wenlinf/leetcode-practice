81ae9c21226271cc1cdd4614b8a891099b60159f
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-08-06 08:59:30 -0400

    Bottom-up solution

--- DynamicProgramming/CombinationSumIV.java
+++ DynamicProgramming/CombinationSumIV.java
@@ -18,3 +18,20 @@ class CombinationSumIV {
         return result;
     }
 }
+
+
+//bottom-up
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        int n = nums.length;
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < n; j++) {
+                if (nums[j] > i) continue;
+                dp[i] += dp[i - nums[j]];
+            }
+        }
+        return dp[target];
+    }
+}
