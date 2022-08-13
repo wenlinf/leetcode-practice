a0e13f9d770d053c10ede984fbb05db684bc58e5
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-08-12 20:06:38 -0400

    Create PathWithMinimumEffort.java
    https://leetcode.com/problems/path-with-minimum-effort/

---
+++ Graph/PathWithMinimumEffort.java
@@ -0,0 +1,43 @@
+//Brute Force backtracking TLE
+class Solution {
+    private int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public int minimumEffortPath(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        this.minEffort = Integer.MAX_VALUE;
+        int currEffort = 0;
+        Set<List<Integer>> visited = new HashSet<>();
+        dfs(heights, m, n, 0, 0, visited, currEffort);
+        return minEffort;
+    }
+    
+    private int minEffort;
+    private void dfs(int[][] heights, int m, int n, int i, int j, Set<List<Integer>> visited, int currEffort) {
+        if (i == m - 1 && j == n - 1) {
+            minEffort = Math.min(minEffort, currEffort);
+            currEffort = 0;
+            return;
+        }
+        List<Integer> list = new ArrayList<>();
+        list.add(i);
+        list.add(j);
+        visited.add(list);
+        for (int[] direction : directions) {
+            int x = direction[0];
+            int y = direction[1];
+            int row = i + x;
+            int col = j + y;
+            if (row >= 0 && row < m && col >= 0 && col < n) {
+                List<Integer> currList = new ArrayList<>();
+                currList.add(row);
+                currList.add(col);
+                if (visited.contains(currList)) continue;
+                int temp = currEffort;
+                currEffort = Math.max(currEffort, Math.abs(heights[row][col] - heights[i][j]));
+                dfs(heights, m, n, row, col, visited,currEffort);
+                currEffort = temp;
+            }
+        }
+        visited.remove(list);
+    }
+}
