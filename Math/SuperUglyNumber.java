34b28eab4ebecf2ec25a8783117793b7ac8c74ea
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-08-15 17:33:09 -0400

    Update SuperUglyNumber.java

--- Math/SuperUglyNumber.java
+++ Math/SuperUglyNumber.java
@@ -18,3 +18,28 @@ class Solution {
         return (int) res;
     }
 }
+
+
+class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (int i = 0; i < primes.length; i++) {
+            pq.add(new int[]{1, primes[i], 1});
+        }
+        int[] ugly = new int[n + 1];
+        int pt = 1;
+        while (pt <= n) {
+            int[] top = pq.poll();
+            int num = top[0];
+            int prime = top[1];
+            int index = top[2];
+            if (num != ugly[pt - 1]) {
+                ugly[pt] = num;
+                pt++;
+            }
+
+            pq.offer(new int[]{prime * ugly[index], prime, index + 1});
+        }
+        return ugly[n];
+    }
+}
