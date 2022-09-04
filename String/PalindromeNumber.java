1e60758767a13fb3168f54a9644e30f3e9e70c71
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-09-04 13:18:02 -0400

    Update PalindromeNumber.java

--- String/PalindromeNumber.java
+++ String/PalindromeNumber.java
@@ -11,3 +11,17 @@ class PalindromeNumber {
        return temp == res;          
     }
 }
+
+// revert the whole number but there might be overflow. because the reverted number might be bigger than Integer.MAX_VALUE
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int reverse = 0;
+        int temp = x;
+        while (x != 0) {
+            reverse = reverse * 10 + x % 10;
+            x = x / 10;
+        }
+        return temp == reverse;
+    }
+}
