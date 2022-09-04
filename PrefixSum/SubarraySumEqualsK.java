315398a35942cd1aea28e365cea716a0226a7305
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-09-04 14:56:04 -0400

    Update SubarraySumEqualsK.java

--- PrefixSum/SubarraySumEqualsK.java
+++ PrefixSum/SubarraySumEqualsK.java
@@ -14,3 +14,21 @@ class Solution {
         return result;
     }
 }
+
+//checked hints, slower than hashmap solution
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int[] prefix = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i - 1];
+        }
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (prefix[j + 1] - prefix[i] == k) result++;
+            }
+        }
+        return result;
+    }
+}
