7a065eac8cf297c707f6a000fa1bc26416f8b7bd
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-09-08 21:04:39 -0400

    sliding window solution

--- String/LongestSubstringWithoutRepeatingCharacters.java
+++ String/LongestSubstringWithoutRepeatingCharacters.java
@@ -68,3 +68,28 @@ class Solution {
         return Math.max(result, sb.length());
     }
 }
+
+// another sliding window that i wrote myself
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        if (n <= 1) return n;
+        int left = 0;
+        int right = left;
+        int result = 1;
+        Map<Character, Integer> map = new HashMap<>();
+        while (right < n) {
+            char rightChar = s.charAt(right);
+            if (map.containsKey(rightChar)) {
+                int index = map.get(rightChar);
+                if (index >= left) {
+                    result = Math.max(result, right - left);
+                    left = index + 1;
+                }
+            }
+            map.put(rightChar, right);
+            right++;
+        }
+        return Math.max(right - left, result);
+    }
+}
