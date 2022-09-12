6a8cc06c1aab5c5a0aef9b31df43c449b3a3d395
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-09-11 20:33:48 -0400

    Update RemoveAllAdjacentDuplicatesinStringII.java

--- String/RemoveAllAdjacentDuplicatesinStringII.java
+++ String/RemoveAllAdjacentDuplicatesinStringII.java
@@ -70,3 +70,26 @@ class Solution {
         return sb.toString();
     }
 }
+
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        if (s.length() == 1) return s;
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Pair<Character, Integer>> stack = new Stack<>();
+        for (int i = 0; i < sb.length(); i++) {
+            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                stack.push(new Pair(sb.charAt(i), 1));
+            } else {
+                Pair<Character, Integer> pair = stack.pop();
+                int newCount = pair.getValue() + 1;
+                if (newCount == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    i = i - k;
+                } else {
+                    stack.push(new Pair(pair.getKey(), newCount));
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
