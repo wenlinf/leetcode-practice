82bf9e3695a4139cda6deb9bef27a9f4555bd29a
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-09-11 20:29:55 -0400

    Create RemoveAllAdjacentDuplicatesinStringII.java
    https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/

---
+++ String/RemoveAllAdjacentDuplicatesinStringII.java
@@ -0,0 +1,72 @@
+//All solutions from leetcode
+//brute force TLE
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        int n = s.length();
+        if (n == 1) return s;
+        StringBuilder sb = new StringBuilder(s);
+        int prevLength = -1;
+        while (prevLength != sb.length()) {
+            prevLength = sb.length();
+            int count = 1;
+            for (int i = 0; i < sb.length(); i++) {
+                if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                    count = 1;
+                } else {
+                    count++;
+                    if (count == k) {
+                        sb.delete(i - count + 1, i + 1);
+                        break;
+                    }
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
+
+// Memoize the counts
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        int n = s.length();
+        if (n == 1) return s;
+        StringBuilder sb = new StringBuilder(s);
+        int prevLength = -1;
+        int[] counts = new int[n];
+        for (int i = 0; i < sb.length(); i++) {
+            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                counts[i] = 1;
+            } else {
+                counts[i] = counts[i - 1] + 1;
+                if (counts[i] == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    i = i - k;
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
+
+// use stack to store counts
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        if (s.length() == 1) return s;
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < sb.length(); i++) {
+            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                stack.push(1);
+            } else {
+                int newCount = stack.pop() + 1;
+                if (newCount == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    i = i - k;
+                } else {
+                    stack.push(newCount);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
