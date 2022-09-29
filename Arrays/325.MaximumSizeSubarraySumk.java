0ecbce0eeb0b67e78a64e2aff9112b3a256cc718
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-09-29 15:51:15 -0400

    Update 325.MaximumSizeSubarraySumk.java

--- Arrays/325.MaximumSizeSubarraySumk.java
+++ Arrays/325.MaximumSizeSubarraySumk.java
@@ -13,3 +13,30 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    /*
+    prefix sum: 
+    arr = 1, 2, 3, 4, 5
+    prefix = 0, 1, 3, 6, 10, 15
+    
+    to get sum from i to j: 
+      sum[i-j] = prefix[j + 1] - prefix[i]
+    */
+    public int maxSubArrayLen(int[] nums, int k) {
+        int n = nums.length;
+        int result = 0;
+        int[] prefix = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i - 1];
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (prefix[j] - prefix[i] == k) {
+                    result = Math.max(result, j - i);
+                }
+            }
+        }
+        return result;
+    }
+}
