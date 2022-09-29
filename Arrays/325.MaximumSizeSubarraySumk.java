7d019ed9d93847c6f1cae0a99ae4b52476402db0
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-09-29 16:09:25 -0400

    Update 325.MaximumSizeSubarraySumk.java

--- Arrays/325.MaximumSizeSubarraySumk.java
+++ Arrays/325.MaximumSizeSubarraySumk.java
@@ -40,3 +40,33 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    /*
+    prefix sum: 
+    arr = 1, 2, 3, 4, 5
+    prefix = 1, 3, 6, 10, 15
+    
+    if k = 5?
+    to get sum from i to j: 
+      sum[i-j] = prefix[j + 1] - prefix[i]
+      1. create a HashMap to store the prefix sums (key is index, value is the prefix sum at that index)
+      2. traverse the array, store the prefix sum in the hashmap on the way
+      3. while traversing, if current prefix - k exist in hashmap, we can update the result
+    */
+    public int maxSubArrayLen(int[] nums, int k) {
+        int n = nums.length;
+        int result = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 0);
+        int prefix = 0;
+        for (int i = 0; i < n; i++) {
+            prefix += nums[i];
+            if (map.containsKey(prefix - k)) {
+                result = Math.max(result, i - map.get(prefix - k) + 1);
+            }
+            if (!map.containsKey(prefix)) map.put(prefix, i + 1);
+        }
+        return result;
+    }
+}
