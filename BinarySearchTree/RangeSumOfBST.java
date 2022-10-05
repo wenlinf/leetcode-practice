8641227f27f75a8f6cda6326361c57e07b749092
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-10-05 09:51:49 -0400

    Update RangeSumOfBST.java

--- BinarySearchTree/RangeSumOfBST.java
+++ BinarySearchTree/RangeSumOfBST.java
@@ -43,3 +43,35 @@ class Solution {
         return nodes;        
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        int sum = 0;
+        if (root == null) return sum;
+          if (root.val > high) {
+            sum = rangeSumBST(root.left, low, high);
+          } else if (root.val < low) {
+            sum = rangeSumBST(root.right, low, high);
+          } else {
+            sum += root.val;
+            sum += rangeSumBST(root.left, low, high);
+            sum += rangeSumBST(root.right, low, high);
+          }
+          return sum;
+    }
+}
