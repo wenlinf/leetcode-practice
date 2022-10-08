b2792716203b01fdcd54eee9b906ad6bb8d9c240
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-10-08 16:01:13 -0400

    Update DesignHitCounter.java

--- Design/DesignHitCounter.java
+++ Design/DesignHitCounter.java
@@ -35,3 +35,55 @@ class HitCounter {
  * obj.hit(timestamp);
  * int param_2 = obj.getHits(timestamp);
  */
+
+// another leetcode solution that is more efficient when multiple hits can happen at the same time
+// Time complexity: hit -> O(1) because we are at most checking the last element in the list, and add a new element to the list
+// getHits -> could be O(n) in the worst case because we might have to remove all the previous elements from the list
+// Space complexity: O(n) because we need an additional list to keep track of all the elements
+// But if there are multiple repetitions, the space and time complexities are actually better because for an element that appeared m times, when it's 
+//removed, it only takes O(1) time rather than O(m) time. For space complexity it's the same
+class HitCounter {
+    private Deque<int[]> hits; // int[] -> {timestamp, count}
+    private int total;
+    private int PREV = 300;
+    
+    public HitCounter() {
+        this.hits = new LinkedList<>();
+        this.total = 0;
+    }
+    
+    public void hit(int timestamp) {
+        if (this.hits.size() != 0) {
+            if (this.hits.getLast()[1] == timestamp) {
+                int[] last = this.hits.removeLast();
+                int freq = last[1];
+                this.hits.add(new int[]{timestamp, freq + 1});
+            } else {
+                this.hits.add(new int[]{timestamp, 1});
+            }
+        } else {
+            this.hits.add(new int[]{timestamp, 1});
+        }
+        this.total++;
+    }
+    
+    public int getHits(int timestamp) {
+        while (this.hits.size() > 0) {
+            if (timestamp - this.hits.getFirst()[0] >= PREV) {
+                int[] first = this.hits.getFirst();
+                this.hits.removeFirst();
+                this.total -= first[1];
+            } else {
+                break;
+            }
+        }
+        return total;
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
