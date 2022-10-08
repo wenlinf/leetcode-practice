4ef7797c8018d536a074bd853f2d802eb082b30b
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-10-08 15:24:11 -0400

    update time complexity analysis
    amortized analysis

--- Design/DesignHitCounter.java
+++ Design/DesignHitCounter.java
@@ -1,5 +1,9 @@
 // Leetcode solution using Queue. Time complexity: getHits -> O(n) in the worst case because we might have to remove all the previous elements
 // hit -> O(1) because we are only adding a number to the queue
+// IMPORTANT: However, we must notice that each timestamp is processed only twice (first while adding the timestamp 
+// in the queue in hit method and second while removing the timestamp from the queue in the getHits method). 
+// Hence if the total number of timestamps encountered throughout is N, the overall time taken by getHits method is 
+// O(N). This results in an amortized time complexity of O(1) for a single call to getHits method.
 // Space complexity: O(n) because we create a queue to store all the timestamps
 class HitCounter {
 
