ab9eb2915a1c7a35534978579a8c451d9d6ec08f
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-11-05 15:35:58 -0400

    Create FurthestBuildingYouCanReach.java
    cleaner leetcode official solution

--- Greedy/FurthestBuildingYouCanReach.java
+++ Greedy/FurthestBuildingYouCanReach.java
@@ -25,3 +25,22 @@ class Solution {
         return -1;
     }
 }
+
+// solution from leetcode official
+class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 1; i < heights.length; i++) {
+            int diff = heights[i] - heights[i - 1];
+            if (diff > 0) {
+                pq.add(diff);
+                if (pq.size() > ladders) {
+                    int min = pq.poll();
+                    bricks -= min;
+                    if (bricks < 0) return i - 1;
+                }
+            }
+        }
+        return heights.length - 1;
+    }
+}
