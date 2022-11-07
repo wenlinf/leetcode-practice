bb22fcd5fe07c314c9bb6bfc968be1c34a59306f
wenlinf <42773608+wenlinf@users.noreply.github.com>
2022-11-06 21:17:29 -0500

    Update DeleteOperationforTwoStrings.java
    bottom-up

--- DynamicProgramming/DeleteOperationforTwoStrings.java
+++ DynamicProgramming/DeleteOperationforTwoStrings.java
@@ -27,3 +27,25 @@ class Solution {
         return dp[i][j];
     }
 }
+
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        // find longest common subsequence
+        // then the result would be (word1.length - LCS) + (word2.length - LCS)
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        int longestSubsequence = dp[m][n];
+        return (m - longestSubsequence) + (n - longestSubsequence);
+    }
+}
