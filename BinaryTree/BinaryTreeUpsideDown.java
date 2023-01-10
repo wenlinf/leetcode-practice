07c177b500cf5c468fe3ad9541e324f26fe60ad3
wenlinf <42773608+wenlinf@users.noreply.github.com>
2023-01-10 08:07:47 -0500

    iterative solution
    https://leetcode.com/problems/binary-tree-upside-down/solutions/49406/java-recursive-o-logn-space-and-iterative-solutions-o-1-space-with-explanation-and-figure/
    check the first comment

--- BinaryTree/BinaryTreeUpsideDown.java
+++ BinaryTree/BinaryTreeUpsideDown.java
@@ -24,3 +24,38 @@ class Solution {
         return newRoot;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root == null || root.left == null) return root;
+        TreeNode prev = null;
+        TreeNode curr = root;
+        TreeNode next;
+        TreeNode temp = null;
+        while (curr != null) {
+            next = curr.left;
+            curr.left = temp;
+            temp = curr.right;
+            curr.right = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
