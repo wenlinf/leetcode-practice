b2e58bffbd1707372a95f2c672854afea0cd0484
wenlinf <42773608+wenlinf@users.noreply.github.com>
2023-01-16 12:32:26 -0500

    backtrack solution

--- BinaryTree/HasPathSum.java
+++ BinaryTree/HasPathSum.java
@@ -29,3 +29,42 @@ class HasPathSum {
         return result;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        backtrack(root, targetSum);
+        return hasSum;
+    }
+
+    private boolean hasSum;
+    private void backtrack(TreeNode root, int targetSum) {
+        if (root == null) return;
+        if (root.val == targetSum) {
+            if (root.left == null && root.right == null) {
+                hasSum = true;
+            return;
+            } 
+        }
+        targetSum -= root.val;
+        backtrack(root.left, targetSum);
+        backtrack(root.right, targetSum);
+        targetSum += root.val;
+    }
+}
