71b4e0ca353535fd6295aab685c39e9ebeba6489
wenlinf <42773608+wenlinf@users.noreply.github.com>
2023-01-16 21:20:28 -0500

    solution based on 5800 homework

--- DynamicProgramming/LongestPalindromicSubsequence.java
+++ DynamicProgramming/LongestPalindromicSubsequence.java
@@ -58,3 +58,28 @@ class Solution {
         return dp[0][n - 1];
     }
 }
+
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        if (n <= 1) return n;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+            int j = i + 1;
+            if (j < n) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) ? 2 : 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 2; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
