4f4a79571023468366bdb01fe8178401e65ebee1
wenlinf <42773608+wenlinf@users.noreply.github.com>
2023-05-16 15:25:48 +0800

    Update SwapNodesInPairs.java
    Iterative solution

--- LinkedList/SwapNodesInPairs.java
+++ LinkedList/SwapNodesInPairs.java
@@ -24,3 +24,36 @@ class SwapNodesInPairs {
         return temp;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = slow.next.next;
+        while (fast != null) {
+            // swap
+            ListNode newNext = fast.next;
+            ListNode temp = slow.next;
+            slow.next = temp.next;
+            slow.next.next = temp;
+            temp.next = newNext;
+            
+            slow = slow.next.next;
+            if (slow == null || (slow.next == null)) break;
+            fast = slow.next.next;
+        }
+        return dummy.next;
+    }
+}
